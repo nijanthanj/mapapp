a35fef427cfc538256bd6173634f6cfdfc5a8271
nijanthanj <nijanthanj35ece@gmail.com>
2017-06-12 10:50:44 +0530

    bookig

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -7,21 +7,28 @@
                 <thead>
                     <tr>
                         <th>Trip ID</th>
-                        <th>Driver ID</th>
+                        <th>Driver</th>
+                        <th>User</th>
+                        <th>User Mobile</th>
                         <th>Pickup</th>
                         <th>Drop-off</th>
+                        <th>No. of pas</th>
+                        <th>KM</th>
                         <th>Trip status</th>                                        
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($booking_list as $booking_list)
-                        <tr>
-                            {{$booking_list}}
-                            <!-- <td>{{$booking_list->trip_id}}</td>
-                            <td>{{$booking_list->driver_id}}</td>
+                        <tr>              
+                            <td>{{$booking_list->trip_id}}</td>
+                            <td>{{$booking_list->driver_name}}</td>
+                            <td>{{$booking_list->user_fname}}</td>
+                            <td>{{$booking_list->mobile}}</td>
                             <td>{{$booking_list->pickup}}</td>
                             <td>{{$booking_list->dropoff}}</td>
-                            <td>{{$booking_list->trip_status}}</td>   -->                                          
+                            <td>{{$booking_list->no_of_pas}}</td>
+                            <td>{{$booking_list->km}}</td>
+                            <td>{{$booking_list->trip_status}}</td>                                         
                         </tr>
                     @endforeach
 
